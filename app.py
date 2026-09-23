c4664116271295dea5bd32d02560a9203667b24f
JAY SHINDE <jay554261@gmail.com>
2026-09-23 12:11:23 +0530

    check cicd works or not

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/health")
 def health():
-    return {"status": "UP"}
+    return {"status": "UP 2"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
