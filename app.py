d4d6ebcb1b95f446bca60b45f421888f27330024
JAY SHINDE <jay554261@gmail.com>
2026-09-23 12:14:45 +0530

    mm

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/health")
 def health():
-    return {"status": "UP 2"}
+    return {"status": "UP 3"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
